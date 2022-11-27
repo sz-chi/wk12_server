2dd6af1e15bdd3e39ab991398a556b342eb5beb2
sz-chi <judy881204@gmail.com>
2022-11-27 23:42:36 +0800

    commit

---
+++ config/allowedOrigins.js
@@ -0,0 +1,10 @@
+const allowedOrigins = [
+    'http://127.0.0.1:5000',
+    'http://localhost:3500',
+    'http://localhost:3000',
+    'http://localhost:3000/Login',
+    'https://longing-head-production.up.railway.app/Login',
+    'https://longing-head-production.up.railway.app'
+];
+
+module.exports = allowedOrigins;
